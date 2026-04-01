f851dd319caef61cf254316897cdb4c67d54d5bc
Juan Mazzola <juan.mazzola@gmail.com>
2026-04-01 19:28:03 -0300

    temp: remove migration runner script

--- public/run-migration.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require __DIR__.'/../bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-Artisan::call('migrate', ['--force' => true]);
-echo nl2br(Artisan::output());
